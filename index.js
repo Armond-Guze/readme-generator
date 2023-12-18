71410c77d475a917e6a978a8db04bf3b6708541e
Armond-Guze <Armond.Guze@yahoo.com>
2023-12-18 00:49:10 -0500

    added first prompt

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is the title of your project',
+    }
+])
