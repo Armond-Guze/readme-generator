f86b29865f35588a0c44b9e6d55047de28c04f45
Armond-Guze <Armond.Guze@yahoo.com>
2023-12-25 17:41:26 -0500

    readme format changes

--- README.md
+++ README.md
@@ -1,28 +1,29 @@
 
-    # Readme Generator
-    https://github.com/armond-guze/
-    
-    ## creating readmes
-    
-    ## Table Of Contents
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [License](#license)
-    - [Contributing](#contributing)
-    - [Tests](#tests)
-    - [Questions](#questions)
-    
-    ## Usage
-    creating readmes
-    
-    ## License
-    
-    
-    ## Contributing
-    armond guze
-    
-    ## Tests
-    install node
-    
-    ## armond-guze
+   # ReadMe Generator
+[GitHub Repository](https://github.com/armond-guze/)
+
+## creating readmes
+
+## Table Of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Usage
+to create a read me
+
+## License
+
+
+## Contributing
+armond guze
+
+## Tests
+no
+
+## Questions
+Contact: [armond-guze](https://github.com/armond-guze)
     
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,32 +1,32 @@
 function generateMarkdown(data) {
   const generateReadme = `
-    # ${data.title}
-    [GitHub Repository](https://github.com/${data.username}/)
-    
-    ## ${data.description}
-    
-    ## Table Of Contents
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [License](#license)
-    - [Contributing](#contributing)
-    - [Tests](#tests)
-    - [Questions](#questions)
-    
-    ## Usage
-    ${data.usage}
-    
-    ## License
-    ${data.license.join(", ")}
-    
-    ## Contributing
-    ${data.contributing}
-    
-    ## Tests
-    ${data.tests}
-    
-    ## Questions
-    Contact: [${data.username}](https://github.com/${data.username})
+   # ${data.title}
+[GitHub Repository](https://github.com/${data.username}/)
+
+## ${data.description}
+
+## Table Of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Usage
+${data.usage}
+
+## License
+${data.license.join(", ")}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+Contact: [${data.username}](https://github.com/${data.username})
     `;
   return generateReadme;
 }
